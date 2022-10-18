732c5cdf331f3dc0ce3d24012178cd58d3a3d04a
Kodajak <ruihsienkwan@gmail.com>
2022-10-18 14:48:21 +0800

    sql scripts

---
+++ SQLScripts/drop.sql
@@ -0,0 +1,9 @@
+-- Don't uncomment unless necessary
+
+-- DROP TABLES
+-- DROP TABLE IF EXISTS LJRole cascade;
+-- DROP TABLE IF EXISTS LearningJourney cascade;
+-- DROP TABLE IF EXISTS Role cascade;
+-- DROP TABLE IF EXISTS Staff cascade;
+-- DROP TABLE IF EXISTS Course cascade;
+-- DROP TABLE IF EXISTS Skill cascade;
