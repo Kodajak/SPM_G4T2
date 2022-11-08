1461e9eaa4352332d3efc9796756bb66d9a30eac
Sam Yu Puay <samyu.puay.2020@smu.edu.sg>
2022-11-08 23:30:29 +0800

    delete files

--- SQLScripts/drop.sql
+++
@@ -1,9 +0,0 @@
--- Don't uncomment unless necessary
-
--- DROP TABLES
--- DROP TABLE IF EXISTS LJRole cascade;
--- DROP TABLE IF EXISTS LearningJourney cascade;
--- DROP TABLE IF EXISTS Role cascade;
--- DROP TABLE IF EXISTS Staff cascade;
--- DROP TABLE IF EXISTS Course cascade;
--- DROP TABLE IF EXISTS Skill cascade;
