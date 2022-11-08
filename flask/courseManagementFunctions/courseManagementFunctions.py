6f0fd6ef88a0978e0b1719d3be69ee01df12f62a
Sam Yu Puay <samyu.puay.2020@smu.edu.sg>
2022-11-08 23:04:56 +0800

    reset main

--- flask/courseManagementFunctions/courseManagementFunctions.py
+++ flask/courseManagementFunctions/courseManagementFunctions.py
@@ -4,7 +4,6 @@ import mysql.connector as mysql
 import pandas as pd
 import os
 
-
 courseManagementFunctions = Blueprint('courseManagementFunctions', __name__)
 
 # enter your server IP address/domain name
@@ -27,47 +26,6 @@ CORS(courseManagementFunctions)
 
 # --------- Course Management Functions ---------
 # -------------------- Start --------------------
-
-# [START] Function to convert CSV file to insert data into Database
-def parseCSV(filePath):
-   # CVS Column Names
-    col_names = ['Course_ID', 'Course_Name', 'Course_Desc', 'Course_Status', 'Course_Type', 'Course_Category']
-    # Use Pandas to parse the CSV file
-    csvData = pd.read_csv(filePath, names = col_names, header = None, encoding= 'unicode_escape', skiprows=1)
-    # Loop through the Rows
-
-    for i, row in csvData.iterrows():
-        sql = "INSERT INTO Course_Test (Course_ID, Course_Name, Course_Desc, Course_Status, Course_Type, Course_Category) VALUES (%s, %s, %s, %s, %s, %s)"
-        value = (row['Course_ID'], row['Course_Name'], row['Course_Desc'], row['Course_Status'], row['Course_Type'], row['Course_Category'])
-        cursor.execute(sql, value)
-        db_connection.commit()
-        print(value)
-# [END] Function to convert CSV file to insert data into Database
-
-# [START] Function to IMPORT all courses from CSV file
-@courseManagementFunctions.route("/import_csv", methods=['POST'])
-def uploadFiles():
-   # get the uploaded file
-    if('file' in request.files):
-        uploaded_file = request.files['file']
-        if uploaded_file.filename == 'courses.csv':
-            # set the file path
-            file_path = os.path.join(courseManagementFunctions.config['UPLOAD_FOLDER'], uploaded_file.filename)
-            # save the file
-            uploaded_file.save(file_path)
-            try:
-                parseCSV(file_path)
-                return redirect("http://localhost:8888/SPMProject/SPM%2520Project/htdocs/statusOfImportSuc.html")
-            except mysql.errors.IntegrityError:
-                return redirect("http://localhost:8888/SPMProject/SPM%2520Project/htdocs/statusOfImportDup.html")
-            except Exception:
-                return redirect("http://localhost:8888/SPMProject/SPM%2520Project/htdocs/statusOfImportUnCom.html")
-        else:
-                return redirect("http://localhost:8888/SPMProject/SPM%2520Project/htdocs/statusOfImportOnl.html")
-    else:
-        return redirect("http://localhost:8888/SPMProject/SPM%2520Project/htdocs/coursesManagement.html")
-
-# [END] Function to IMPORT all courses from CSV file
 
 # [START] Function to Redirect and show CSV Import Status on HTML
 @courseManagementFunctions.route("/statusOfImportUnCom")
--- flask/mappingFunctions/test_mapping.py
+++ flask/mappingFunctions/test_mapping.py
@@ -19,32 +19,13 @@ cursor = db_connection.cursor()
 class TestSkillMapping(unittest.TestCase):
 
     def setUp(self):
-        query = "INSERT INTO Skill (skill_id, skill_name, skill_desc, status) VALUES (999998, 'Skill to Delete 2', 'Skill to Delete Description', 1)"
-        cursor.execute(query)
-        db_connection.commit()
-
-        query = "INSERT INTO Course (course_id, course_name, course_desc, course_status, course_type, course_category) VALUES (001, 'Course to Delete', 'Course to Delete Description', 1)"
-        cursor.execute(query)
-        db_connection.commit()
-
-        query = "INSERT INTO Course_Skill (course_id, skill_id) VALUES (001, 999998)"
-        cursor.execute(query)
-        db_connection.commit()
-
-        query = "INSERT INTO LJRole (ljrole_id, ljrole_name, ljrole_desc, status) VALUES (01, 'LJRole to Delete', 'LJRole to Delete Description', 1)"
-        cursor.execute(query)
-        db_connection.commit()
-
-        query = "INSERT INTO LJRole_Skill (ljrole_id, skill_id) VALUES (01, 999998)"
-        cursor.execute(query)
-        db_connection.commit()
 
         return super().setUp()
     
     def tearDown(self):
-        query = 'DELETE FROM Skill WHERE skill_name="Skill to Delete 2"' 
-        cursor.execute(query)
-        db_connection.commit()
+        # query = 'DELETE FROM Skill WHERE skill_name="Skill to Delete 2"' 
+        # cursor.execute(query)
+        # db_connection.commit()
 
         # query = 'DELETE FROM Skill WHERE skill_name="Edited Skill Name"' 
         # cursor.execute(query)
@@ -65,7 +46,27 @@ class TestSkillMapping(unittest.TestCase):
         return super().tearDown()
 
     def test_view_skill_mapping(self):
-        r = requests.get('http://localhost:5000/view-skill-mapping/999999')
+        query = "INSERT INTO Skill (skill_id, skill_name, skill_desc, status) VALUES (9999, 'Skill to Delete 2', 'Skill to Delete Description', 1)"
+        cursor.execute(query)
+        db_connection.commit()
+
+        query = "INSERT INTO Course (course_id, course_name, course_desc, course_status, course_type, course_category) VALUES (1111, 'Course to Delete', 'Course to Delete Description', 'Active', 'Internal', 'course to delete category')"
+        cursor.execute(query)
+        db_connection.commit()
+
+        query = "INSERT INTO Course_Skill (course_id, skill_id) VALUES (1111, 9999)"
+        cursor.execute(query)
+        db_connection.commit()
+
+        query = "INSERT INTO LJRole (ljrole_id, ljrole_name, ljrole_desc, status) VALUES (2222, 'LJRole to Delete', 'LJRole to Delete Description', 1)"
+        cursor.execute(query)
+        db_connection.commit()
+
+        query = "INSERT INTO LJRole_Skill (ljrole_id, skill_id) VALUES (2222, 9999)"
+        cursor.execute(query)
+        db_connection.commit()
+
+        r = requests.get('http://localhost:5000/view-skill-mapping/9999')
         # Does this work?^^^ No skillID indicated
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.encoding, 'utf-8')
@@ -73,12 +74,32 @@ class TestSkillMapping(unittest.TestCase):
         self.assertEqual(type(r.json()['roles']), list)
         self.assertEqual(type(r.json()['courses']), list)
 
-    def test_update_skill_mapping(self):
-        r = requests.get("http://localhost:5000/update-skill-mapping/999999")
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.encoding, 'utf-8')
-        self.assertEqual(type(r.json()['skill']), str)
-        self.assertEqual(type(r.json()['roles']), list)
-        self.assertEqual(type(r.json()['courses']), list)
-        self.assertEqual(type(r.json()['currentMappedRoles']), list)
-        self.assertEqual(type(r.json()['currentMappedCourses']), list)
+        query = "DELETE FROM LJRole_Skill WHERE ljrole_id = 2222"
+        cursor.execute(query)
+        db_connection.commit()
+
+        query = "DELETE FROM Course_Skill WHERE course_id = 1111"
+        cursor.execute(query)
+        db_connection.commit()
+
+        query = "DELETE FROM LJRole WHERE ljrole_id = 2222"
+        cursor.execute(query)
+        db_connection.commit()
+
+        query = "DELETE FROM Course WHERE course_id = 1111"
+        cursor.execute(query)
+        db_connection.commit()
+
+        query = "DELETE FROM Skill WHERE skill_id=9999"
+        cursor.execute(query)
+        db_connection.commit()
+
+    # def test_update_skill_mapping(self):
+    #     r = requests.get("http://localhost:5000/update-skill-mapping/999999")
+    #     self.assertEqual(r.status_code, 200)
+    #     self.assertEqual(r.encoding, 'utf-8')
+    #     self.assertEqual(type(r.json()['skill']), str)
+    #     self.assertEqual(type(r.json()['roles']), list)
+    #     self.assertEqual(type(r.json()['courses']), list)
+    #     self.assertEqual(type(r.json()['currentMappedRoles']), list)
+    #     self.assertEqual(type(r.json()['currentMappedCourses']), list)
